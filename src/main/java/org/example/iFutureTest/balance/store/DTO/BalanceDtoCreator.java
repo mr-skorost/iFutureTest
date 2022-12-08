b9dd9657e4d05f907a04a78058f7e64f7c6fd98f
Stanislav <skorenko.stanislav@gmail.com>
2022-12-08 22:36:17 +0300

    Init Balance dto creater

--- src/main/java/org/example/iFutureTest/balance/store/DTO/BalanceDtoCreator.java
+++ src/main/java/org/example/iFutureTest/balance/store/DTO/BalanceDtoCreator.java
@@ -1,2 +1,16 @@
-package org.example.iFutureTest.balance.store.DTO;public class BalanceDtoCreator {
+package org.example.iFutureTest.balance.store.DTO;
+
+import org.example.iFutureTest.balance.store.entity.BalanceEntity;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BalanceDtoCreator {
+
+    public BalanceDTO makeBalanceDTO(BalanceEntity entity){
+        return BalanceDTO.builder()
+                .id(entity.getId())
+                .balance(entity.getId())
+                .build();
+    }
 }
