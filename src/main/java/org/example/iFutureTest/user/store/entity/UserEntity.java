8eae66906c36e43abc2e1d2a8b3bcf469f1d5abf
Stanislav <skorenko.stanislav@gmail.com>
2022-12-08 22:41:17 +0300

    init User

--- src/main/java/org/example/iFutureTest/user/store/entity/UserEntity.java
+++ src/main/java/org/example/iFutureTest/user/store/entity/UserEntity.java
@@ -1,4 +1,27 @@
 package org.example.iFutureTest.user.store.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.iFutureTest.balance.store.entity.BalanceEntity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<BalanceEntity> balances;
+
 }
