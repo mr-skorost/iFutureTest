1c4fa6e1a2f67888a7199a9c6d938e65d3098fa1
Stanislav <skorenko.stanislav@gmail.com>
2022-12-08 22:36:35 +0300

    balance dto

--- src/main/java/org/example/iFutureTest/balance/store/DTO/BalanceDTO.java
+++ src/main/java/org/example/iFutureTest/balance/store/DTO/BalanceDTO.java
@@ -1,4 +1,19 @@
 package org.example.iFutureTest.balance.store.DTO;
 
+import lombok.*;
+import org.example.iFutureTest.user.store.DTO.UserDTO;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BalanceDTO {
+    @NonNull
+    private Long id;
+
+    private Long balance;
+
+    @NonNull
+    private UserDTO user;
+
 }
