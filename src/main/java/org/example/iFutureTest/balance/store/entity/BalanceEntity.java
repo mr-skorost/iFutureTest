573571ac68a6b7d821a97cd9a45aaa6dfec37d82
Stanislav <skorenko.stanislav@gmail.com>
2022-12-08 22:33:22 +0300

    Init Balance

--- src/main/java/org/example/iFutureTest/balance/store/entity/BalanceEntity.java
+++ src/main/java/org/example/iFutureTest/balance/store/entity/BalanceEntity.java
@@ -1,4 +1,33 @@
 package org.example.iFutureTest.balance.store.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.iFutureTest.user.store.entity.UserEntity;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
 public class BalanceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private Long balance;
+
+    @ManyToOne (optional=false, cascade=CascadeType.ALL)
+    private UserEntity userEntity;
+
+    public BalanceEntity(Long id, Long balance, UserEntity userEntity) {
+        this.id = id;
+        this.balance = balance;
+        this.userEntity = userEntity;
+    }
+
+
 }
